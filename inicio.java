adc55be43a89ab7ebed8d82881c1a42c52038dc9
Sergio Eduardo Arboleda Villa <sarboleda@gmail.com>
2014-01-31 14:50:22 -0500

    Create inicio.java

---
+++ inicio.java
@@ -0,0 +1,4 @@
+import javax.swing.*;
+
+//aplicacion que lee codigo de barras pertenecientes a los recibos del servicio publico de energia empresa ENELAR
+//Lee, verifica en BD, si se encuentra cliente arroja información, delo contrario solicita Nombre y Direccion
